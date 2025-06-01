adb74b43438bc0bf1843d259a35a7454152c226f
AlexanderRyan <alexanderryan98@gmail.com>
2025-06-01 15:55:53 +0100

    Added Header and footer components

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header style={{ padding: '1rem', background: '#20232a', color: 'white' }}>
+      <h1>My Cool Website</h1>
+    </header>
+  );
+}
